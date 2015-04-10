1654bd2253413471d91285ba94d107c5d87862a7
alu0100713435 <alu0100713435@ull.edu.es>
2015-04-10 14:35:31 +0000

    Terminando con las contraseñas

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: "Example User", email: "user@example.com")
-	end
+    	@user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 	
 	test "should be valid" do
 		assert @user.valid?
@@ -46,4 +47,9 @@ class UserTest < ActiveSupport::TestCase
 		@user.save
 		assert_not duplicate_user.valid?
 	end
+	
+	test "password should have a minimum length" do
+	    @user.password = @user.password_confirmation = "a" * 5
+	    assert_not @user.valid?
+  end
 end
