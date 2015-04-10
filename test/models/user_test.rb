9b1cf79ddee48213c143a9d43686fe795f2dbd7f
alu4437 <alu0100713435@ull.edu.es>
2015-04-10 12:23:55 +0100

    Validacion de usuarios

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,11 +32,18 @@ class UserTest < ActiveSupport::TestCase
   	end
 
   	test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
-    valid_addresses.each do |valid_address|
-      @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should be valid"
-    end
-  end
+    		valid_addresses.each do |valid_address|
+      		@user.email = valid_address
+      		assert @user.valid?, "#{valid_address.inspect} should be valid"
+	 end
+	end
+
+	test "email addresses should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
 end
