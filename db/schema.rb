c8730f0defeaea0d629f900f6e0696df273341cd
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-04-13 14:24:01 +0100

    arreglando fallos test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410142704) do
+ActiveRecord::Schema.define(version: 20150413132307) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,7 +20,5 @@ ActiveRecord::Schema.define(version: 20150410142704) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,7 @@ class UserTest < ActiveSupport::TestCase
 	def setup
     	@user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
-  end
+ 	end
 	
 	test "should be valid" do
 		assert @user.valid?
@@ -14,31 +14,29 @@ class UserTest < ActiveSupport::TestCase
 		@user.name= "     "
 		assert_not @user.valid?
 	end
+
 	test "email should be present" do
-	@user.email = "     "
-	assert_not @user.valid?
+		@user.email = "     "
+		assert_not @user.valid?
 	end
 
 	test "name should not be too long" do
     		@user.name = "a" * 51
     		assert_not @user.valid?
-
   	end
 
   	test "email should not be too long" do
     		@user.email = "a" * 244 + "@example.com"
     		assert_not @user.valid?
-
-
   	end
 
   	test "email validation should accept valid addresses" do
     		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     		valid_addresses.each do |valid_address|
-      		@user.email = valid_address
-      		assert @user.valid?, "#{valid_address.inspect} should be valid"
-	 end
+      			@user.email = valid_address
+      			assert @user.valid?, "#{valid_address.inspect} should be valid"
+	 		end
 	end
 
 	test "email addresses should be unique" do
@@ -51,5 +49,5 @@ class UserTest < ActiveSupport::TestCase
 	test "password should have a minimum length" do
 	    @user.password = @user.password_confirmation = "a" * 5
 	    assert_not @user.valid?
-  end
+  	end
 end
