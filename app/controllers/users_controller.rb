8863079ae89df8af09b6c15ef747f3f28de70653
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-04-21 12:49:22 +0100

    mejorando cosas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,34 +6,26 @@ class UsersController < ApplicationController
 
   end
 
-def new
-@user = User.new
+  def new
+    @user = User.new
   end
 
-def create
+  def create
 
     @user = User.new(user_params)
     if @user.save
-	flash[:success] = "Welcome to the Sample App!"
-	redirect_to @user
-
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-
       render 'new'
-
     end
 
   end
   
+
 private
 
-
-
-    def user_params
-
-      params.require(:user).permit(:name, :email, :password,
-
-                                   :password_confirmation)
-
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
