089df1771e8d82235698283275e8ac8704c68cd8
Raquel Alvarez <alu0100700435@ull.edu.es>
2015-04-28 12:59:31 +0000

    cambios

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -21,18 +22,16 @@ module SessionsHelper
     end
   end
   
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-  
   def logged_in?
     !current_user.nil?
   end
+  
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+  
   def log_out
     forget(current_user)
     session.delete(:user_id)
