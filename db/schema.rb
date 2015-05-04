ec25c45c0615d6ecb9386eaf8160cf9dd7d596c5
alu01007134351 <noelpadron1415@gmail.com>
2015-05-04 09:50:18 +0000

    Acabado 9.45

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428130946) do
+ActiveRecord::Schema.define(version: 20150428111851) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
