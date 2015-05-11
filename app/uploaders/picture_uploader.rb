6b6160bb61c3c88e0d393458bb9c80d139fcbd71
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-05-11 13:52:53 +0100

    cambios en uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,23 +1,20 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-  
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-  
-  
-  if Rails.env.production?
+
+  # Choose what kind of storage to use for this uploader:
+   if Rails.env.production?
     storage :fog
   else
     storage :file
   end
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -47,17 +44,14 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+ def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
 
 end
