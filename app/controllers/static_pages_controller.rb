9077362894cb1d6b0d47a2ec95049903552378d9
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-05-13 17:28:05 +0100

    add things

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 15)
-
     end
   end
 
