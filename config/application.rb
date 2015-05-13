faee786bf0c90bbd4824f3efeccbb79deecb9f83
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2015-05-13 18:13:40 +0100

    cambiando carpeta

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module SocialMusic
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/media"
+    config.assets.precompile += %w( .svg )
 
   end
 end
